ac9d135f55fdf7375e58bfc4f9b7a66ae92f6a79
ayon008 <shariar.ayon128@gmail.com>
2025-02-10 16:11:34 +0600

    a

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'https://www.janitorialappointment.com');
