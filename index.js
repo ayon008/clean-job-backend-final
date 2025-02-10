db471fc234958272e72282980f2d4dc8b8b5a3d9
ayon008 <shariar.ayon128@gmail.com>
2025-02-10 16:04:16 +0600

    a

--- index.js
+++ index.js
@@ -27,6 +27,13 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://www.janitorialappointment.com');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next();
+});
+
 app.get('/', (req, res) => {
     res.send('server is running');
 })
